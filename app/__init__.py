3963c754c429cdc80b815178514a57bd7a3c2605
xiaozhen cao <caoxiaozhen@meican-inc.com>
2020-05-22 16:11:08 +0800

    add dockerfile

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @license : Copyright(C), meican-inc
+# @Time    : 2020/5/22 14:09
+# @Contact : caoxiaozhen@100tal.com
+# @Site    : https://www.100tal.com/
+# @File    : __init__.py.py
